a115c480fd65927c8a97adcff17b71e9080cafce
steevehook <steevehook@gmail.com>
2019-11-28 19:07:35 +0200

    add hello-world program

--- section_1-introduction/hello-world/main.go
+++ section_1-introduction/hello-world/main.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+const (
+	hello       = "你好"
+	exclamation = "!"
+)
+
+var world string
+
+func 世界() {
+	world = "世界"
+}
+
+func init() {
+	世界()
+}
+
+func main() {
+	fmt.Println(hello + world + exclamation)
+}
